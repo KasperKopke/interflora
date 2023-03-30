ea8bbda9e5fbf9277599b560d756fc5fe5c156ff
KasperKopke <122533610+KasperKopke@users.noreply.github.com>
2023-03-30 13:26:06 +0200

    done

--- js/app.js
+++ js/app.js
@@ -37,15 +37,76 @@ for (let index2 = 0; index2 < tess.length; index2++) {
 
     if (parentSection2) {
       parentSection2.classList.toggle("active");
-      fun(parentSection2);
     }
   });
 }
 
-function fun(parentSection2) {
-  if (parentSection2.classList.contains("active")) {
-    document.getElementById("testbtn").innerHTML = "Vis Mindre";
-  } else {
-    document.getElementById("testbtn").innerHTML = "Vis Mere";
+function validate() {
+  let form = document.forms["formm"];
+  let errorbox = document.querySelector(".error-message");
+  let textField = document.querySelector("textarea");
+
+  const errorMessages = [];
+
+  if (form["name"].value == "") {
+    errorMessages.push("Name is Required |");
   }
+
+  if (form["emne"].value == "") {
+    errorMessages.push("Emne is Required |");
+  }
+
+  if (form["mail"].value == "") {
+    errorMessages.push("Mail is Required |");
+  } else {
+    var emaildotdkvalidering = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[dk]{2,}$/;
+    if (!emaildotdkvalidering.test(form["mail"].value)) {
+      errorMessages.push(
+        "Please enter a valid email address that ends with .dk |"
+      );
+    }
+  }
+
+  if (textField.value.length > 250) {
+    console.log("hej");
+    errorMessages.push("Max words is reached!! max words is (250) |");
+  }
+
+  if (errorMessages.length != 0) {
+    errorbox.classList.add("show");
+    errorbox.innerText = errorMessages.toString();
+    return false;
+  }
+
+  return false;
+}
+
+console.log("hej");
+
+let selectedImage = "";
+
+const bill = document.querySelectorAll(".thumbnail");
+
+for (let index4 = 0; index4 < bill.length; index4++) {
+  bill[index4].addEventListener("click", (data) => {
+    console.log(data.target.src);
+    changeImg(data.target.src.replace());
+    billCheck(data.target);
+  });
+}
+
+function changeImg(url) {
+  document.getElementById("largeImg").setAttribute("src", url);
+}
+
+function billCheck(clickedElement) {
+  if (selectedImage) {
+    selectedImage.style.border = "none";
+  }
+  selectedImage = clickedElement;
+  selectedImage.style.border = "4px #d1ad53 solid";
+}
+
+function burgerMenu() {
+  document.querySelector("nav").classList.toggle("active");
 }
